8645fe09678cdd083abbed895913b1b2948865b3
Max Ryan <maxryan12345@hotmail.com>
2015-11-13 00:44:09 +0000

    Added Iterative solution to fibonacci sequence

--- fibonacci.groovy
+++ fibonacci.groovy
@@ -9,3 +9,16 @@ int fib(int n) {
 	}
 }
 
+println fib(10)
+
+//Iterative method for Fibonacci sequence
+int fibIt(int n) {
+	if ((n ==1) || (n ==2)) {
+		return 1;
+	} else {
+		for (i = 1; i < n; i++)
+			result = n + (n-1)
+	}
+}
+
+println fib(10)
